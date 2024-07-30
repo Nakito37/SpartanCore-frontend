204a1e2479bc89f5c130d6c3881fd92d16a015f8
Nakito37 <mazin37.fa@gmail.com>
2024-07-30 17:38:42 -0600

    add pages; components templates, add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { UserChannelComponent } from './pages/user-channel/user-channel.component';
+import { MainComponent } from './pages/main/main.component';
+import { VerifyCodeComponent } from './components/verify-code/verify-code.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { UploadContentComponent } from './components/upload-content/upload-content.component';
+import { HeadderComponent } from './components/headder/headder.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+/* ANGULAR MATERIAL MODULES */
+
+/* PRIMENG MODULES */
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    UserChannelComponent,
+    MainComponent,
+    VerifyCodeComponent,
+    ResetPasswordComponent,
+    UploadContentComponent,
+    HeadderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
+    /* ANGULAR MATERIAL MODULES */
+
+    /* PRIMENG MODULES */
+
+    
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/user-channel/user-channel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-channel',
+  templateUrl: './user-channel.component.html',
+  styleUrl: './user-channel.component.css'
+})
+export class UserChannelComponent {
+
+}
